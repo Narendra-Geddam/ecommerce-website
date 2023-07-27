bac5c67281e18022e65b179921e9f99d1ed3995d
Narendra-Geddam <narendrageddam95@gmail.com>
2023-07-27 23:47:29 +0530

    feat: eCommarceWebsite conected with api

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Header from './components/Header';
 import ProductList from './components/ProductList';
@@ -6,18 +6,29 @@ import ProductDetails from './components/ProductDetails';
 import ShoppingCart from './components/ShoppingCart';
 import Checkout from './components/Checkout';
 import NotFound from './components/NotFound';
-// import Footer from './components/Footer';
 import './styles/main.min.css';
 
 const App = () => {
+  const [cartItems, setCartItems] = useState([]);
+
+  const handleAddToCart = (product) => {
+    setCartItems((prevCartItems) => [...prevCartItems, product]);
+  };
+
   return (
     <Router>
-      <Header />
+      <Header cartItems={cartItems} />
       <Switch>
-        <Route exact path="/" component={ProductList} />
+        <Route exact path="/">
+          <ProductList handleAddToCart={handleAddToCart} />
+        </Route>
         <Route path="/product/:id" component={ProductDetails} />
-        <Route path="/cart" component={ShoppingCart} />
-        <Route path="/checkout" component={Checkout} />
+        <Route path="/cart">
+          <ShoppingCart cartItems={cartItems} />
+        </Route>
+        <Route path="/checkout">
+          <Checkout cartItems={cartItems} />
+        </Route>
         <Route component={NotFound} />
       </Switch>
     </Router>
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,13 +1,41 @@
-import React from 'react';
-import productsData from '../data/products';
-import ProductCard from './ProductCard';
+import React, { useState, useEffect } from 'react';
+
+const fetchProducts = () => {
+  return fetch('https://fakestoreapi.com/products')
+    .then((res) => res.json())
+    .then((data) => data);
+};
 
 const ProductList = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetchProducts()
+      .then((data) => setProducts(data))
+      .catch((error) => console.error('Error fetching products:', error));
+  }, []);
+
+  const handleAddToCart = (product) => {
+    // Add product to cart logic here (You can implement this logic in the App component or create a separate cart state)
+    console.log('Product added to cart:', product);
+  };
+
   return (
-    <div className="product-list">
-      {productsData.map((product) => (
-        <ProductCard key={product.id} product={product} />
-      ))}
+    <div>
+      {products.length > 0 ? (
+        products.map((product) => (
+          <div key={product.id}>
+            <h2>{product.title}</h2>
+            <p>{product.price}</p>
+            <p>{product.category}</p>
+            <p>{product.description}</p>
+            <img src={product.image} alt={product.title} />
+            <button onClick={() => handleAddToCart(product)}>Add to Cart</button>
+          </div>
+        ))
+      ) : (
+        <p>No products available.</p>
+      )}
     </div>
   );
 };
