03fb787ce34d7a3ce990b7d4886a951407f44f39
Narendra-Geddam <narendrageddam95@gmail.com>
2023-07-30 00:53:38 +0530

    feat: empty

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -1,5 +1,8 @@
+// ShoppingCart.js
+
 import React from 'react';
 import Checkout from './Checkout';
+
 
 const ShoppingCart = ({ cartItems, onRemoveFromCart }) => {
   const handleRemoveFromCart = (productId) => {
@@ -12,25 +15,27 @@ const ShoppingCart = ({ cartItems, onRemoveFromCart }) => {
   };
 
   return (
-    <div className="cart">
-      <h2>Shopping Cart</h2>
-      {cartItems.length === 0 ? (
-        <p>Your cart is empty.</p>
-      ) : (
-        <div>
-          <ul>
-            {cartItems.map((item) => (
-              <li key={item.id}>
-                <h3>{item.title}</h3>
-                <p>Price: ${item.price}</p>
-                <button onClick={() => handleRemoveFromCart(item.id)}>Remove</button>
-              </li>
-            ))}
-          </ul>
-          <h3>Total: ${getTotalCost()}</h3>
-          <Checkout />
-        </div>
-      )}
+    <div className="page-container">
+      <div className="cart">
+        <h2>Shopping Cart</h2>
+        {cartItems.length === 0 ? (
+          <p>Your cart is empty.</p>
+        ) : (
+          <div>
+            <ul>
+              {cartItems.map((item) => (
+                <li key={item.id}>
+                  <h3>{item.title}</h3>
+                  <p>Price: ${item.price}</p>
+                  <button onClick={() => handleRemoveFromCart(item.id)}>Remove</button>
+                </li>
+              ))}
+            </ul>
+            <h3 className="total">Total: ${getTotalCost()}</h3>
+            <Checkout />
+          </div>
+        )}
+      </div>
     </div>
   );
 };
