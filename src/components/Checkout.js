99b483ae764a7775e9228490f2c532f299a59bc9
Narendra-Geddam <narendrageddam95@gmail.com>
2023-07-30 01:19:58 +0530

    feat : checkout

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
 
 const Checkout = () => {
+  const [name, setName] = useState('');
   const [address, setAddress] = useState('');
+  const [contactNumber, setContactNumber] = useState('');
+  const [email, setEmail] = useState('');
   const [isSubmitted, setIsSubmitted] = useState(false);
 
   const handleSubmit = (e) => {
@@ -14,18 +17,48 @@ const Checkout = () => {
     setAddress(e.target.value);
   };
 
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleContactNumberChange = (e) => {
+    setContactNumber(e.target.value);
+  };
+
+  const handleChangeEmail = (e) => {
+    setEmail(e.target.value);
+  };
+
   return (
-    <div>
+    <div className="checkout-container">
       {isSubmitted ? (
-        <div>
-          <p>Your order will be delivered soon to:</p>
-          <p>{address}</p>
+        <div className="submitted-details">
+          <p>Your order will be delivered soon. Details:</p>
+          <p>Name: {name}</p>
+          <p>Address: {address}</p>
+          <p>Contact Number: {contactNumber}</p>
+          <p>Email: {email}</p>
         </div>
       ) : (
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit} className="checkout-form">
+          <label>
+            Name:
+            <input type="text" value={name} onChange={handleNameChange} />
+          </label>
+          <br />
           <label>
             Address:
             <input type="text" value={address} onChange={handleChange} />
+          </label>
+          <br />
+          <label>
+            Contact Number:
+            <input type="text" value={contactNumber} onChange={handleContactNumberChange} />
+          </label>
+          <br />
+          <label>
+            Email:
+            <input type="text" value={email} onChange={handleChangeEmail} />
           </label>
           <br />
           <button type="submit">Submit</button>
