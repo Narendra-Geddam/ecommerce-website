6bc64e52e29229d3dbb882c011c20d5a7c7ac94b
Narendra-Geddam <narendrageddam95@gmail.com>
2023-07-29 23:03:32 +0530

    fix: resolving conflits

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+//! OG Creator - @narendra
+
+// import useState and useEffect for state of cart and products
+
 import React, { useState, useEffect } from 'react';
+
+//  import router and route and switch for site no reload 
+
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+// importing components from components directory
+
+
+
 import Header from './components/Header';
 import ProductList from './components/ProductList';
 import ProductDetails from './components/ProductDetails';
@@ -7,6 +19,15 @@ import ShoppingCart from './components/ShoppingCart';
 import Checkout from './components/Checkout';
 import NotFound from './components/NotFound';
 import { fetchProducts } from './data/products';
+import { StyleSheet, View } from '@react-pdf/renderer';
+
+
+const styles = StyleSheet.create({
+  bodyFStyle:{
+    fontFamily: 'Arial, sans-serif',
+    color: '#333',
+  }
+});
 
 const App = () => {
   const [products, setProducts] = useState([]);
@@ -52,6 +73,7 @@ const App = () => {
 
   return (
     <Router>
+   <View style={styles.bodyFStyle}>
       <Header onSearch={handleSearch} cartItemCount={cart.length} onSortChange={handleSortChange} />
       <Switch>
         <Route exact path="/">
@@ -64,6 +86,7 @@ const App = () => {
         <Route path="/checkout" component={Checkout} />
         <Route component={NotFound} />
       </Switch>
+    </View>
     </Router>
   );
 };
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,91 @@
+//! OG Creator - @narendra
+
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+
+// Importing Logo Image to Header
+import logoImage from '../assets/images/logo.png';
+
+// importing StyleSheet and View for adding elements
+import { StyleSheet, View,} from '@react-pdf/renderer';
+const styles = StyleSheet.create({
+
+  headerStyle :{
+    display: 'flex',
+    alignItems: 'center',
+    backgroundColor: '#f0f0f0',
+    padding: '10px',
+  },
+
+  logoSection:{
+    display: 'flex',
+    alignItems: 'center',
+  },
+
+  logoStyle : {
+    flex: 1,
+    padding: '5px',
+    width:50,
+    height:50,
+    marginRight:30,
+  },
+
+  brandName:{
+    fontSize:32,
+    fontWeight:800,
+    marginLeft:30,
+  },
+
+  navItemsStyle : {
+    flex: 3,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    fontSize:24,
+  },
+
+  ulStyle : {
+    listStyle: 'none',
+    margin: 0,
+    padding: 0,
+  },
+
+  liStyle : {
+    display: 'inline',
+    marginRight: '20px',
+  },
+
+  searchSortStyle : {
+    display: 'flex',
+    alignItems: 'center',
+  },
+
+  searchBar:{
+    marginLeft:100,
+  },
+
+  searchBarStyle:{
+    fontStyle:16,
+    border:'1px solid #ccc',
+    borderRadius:'50%',
+    backgroundColor:'#f0f0f0'
+  },
+
+  sortOption:{
+
+  },
+
+  selectStyle : {
+    marginRight: '10px',
+    fontSize:16,
+  },
+
+  linkStyle:{
+    textDecoration:'none'
+  }
+
+
+});
 
 const Header = ({ onSearch, cartItemCount, onSortChange }) => {
   const [searchTerm, setSearchTerm] = useState('');
@@ -19,28 +105,45 @@ const Header = ({ onSearch, cartItemCount, onSortChange }) => {
   };
 
   return (
-    <header>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
+    <header style={styles.headerStyle}>
+      <View style={styles.logoSection}>
+        <View style={styles.logoStyle}>
+          {/* logo component  */}
+          <img src={logoImage} alt="Logo" style={styles.logoStyle} />
+        </View>
+        <View style={styles.brandName}>
+        SuperMartX
+        </View>
+      </View>
+      <nav style={styles.navItemsStyle}>
+
+         {/* Search and Sort option */}
+        <View style={styles.searchSortStyle}>
+          <View style={styles.searchBar}>
+            <input style={styles.searchBarStyle} type="text" value={searchTerm} onChange={handleChange} />
+            <button onClick={handleSearch}>Search</button>
+          </View>
+
+          <View style={styles.sortOption}>
+            {/* Add the sorting select element */}
+            <select style={styles.selectStyle} value={sortOption} onChange={handleSortChange}>
+              <option value="asc">High to Low</option>
+              <option value="desc">Low to High</option>
+            </select>
+          </View>
+        </View>
+
+        <ul style={styles.ulStyle}>
+          <li style={styles.liStyle}>
+            <Link style={styles.linkStyle} to="/">Home</Link>
           </li>
-          <li>
-            <Link to="/cart">Cart ({cartItemCount})</Link>
+          <li style={styles.liStyle}>
+            <Link style={styles.linkStyle} to="/cart">Cart ({cartItemCount})</Link>
           </li>
         </ul>
-        <div>
-          {/* Add the sorting select element */}
-          <select value={sortOption} onChange={handleSortChange}>
-            <option value="asc">High to Low</option>
-            <option value="desc">Low to High</option>
-          </select>
-
-          <input type="text" value={searchTerm} onChange={handleChange} />
-          <button onClick={handleSearch}>Search</button>
-        </div>
       </nav>
     </header>
+  
   );
 };
 
